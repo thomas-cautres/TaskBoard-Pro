7405626e04f2bf707b04e75fe396d782d5020dd0
Thomas Cautrès <thomas.cautres@protonmail.com>
2025-10-13 23:02:33 +0200

    --

--- src/Controller/Security/RegistrationController.php
+++ src/Controller/Security/RegistrationController.php
@@ -5,18 +5,16 @@ namespace App\Controller\Security;
 use App\Entity\User;
 use App\Event\Security\UserRegisteredEvent;
 use App\Form\Security\RegistrationType;
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/registration', name: 'registration')]
-    public function __invoke(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, EventDispatcherInterface $dispatcher): Response
+    public function __invoke(Request $request, EventDispatcherInterface $dispatcher): Response
     {
         $user = new User();
 
--- tests/RegistrationTest.php
+++ tests/RegistrationTest.php
@@ -12,10 +12,10 @@ class RegistrationTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/registration');
+        $client->request('GET', '/registration');
         $this->assertResponseIsSuccessful();
 
-        $crawler = $client->submitForm('signup-btn', [
+        $client->submitForm('signup-btn', [
             'registration[email]' => 'test@test.com',
             'registration[password][first]' => 'testtest123',
             'registration[password][second]' => 'testtest123',
@@ -30,5 +30,78 @@ class RegistrationTest extends WebTestCase
         $user = $entityManager->getRepository(User::class)->findOneBy(['email' => 'test@test.com']);
 
         $this->assertInstanceOf(User::class, $user);
+    }
+
+    public function testWithInvalidEmail(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/registration');
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('signup-btn', [
+            'registration[email]' => 'test',
+            'registration[password][first]' => 'testtest123',
+            'registration[password][second]' => 'testtest123',
+        ]);
+
+        $this->assertResponseStatusCodeSame(422);
+    }
+
+    public function testWithMismatchedPasswords(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/registration');
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('signup-btn', [
+            'registration[email]' => 'test@test.com',
+            'registration[password][first]' => 'testtest123',
+            'registration[password][second]' => 'testtest',
+        ]);
+
+        $this->assertResponseStatusCodeSame(422);
+        $this->assertSelectorTextContains('.invalid-feedback', 'Les mots de passe doivent correspondre.');
+    }
+
+    public function testWithWeakPassword(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/registration');
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('signup-btn', [
+            'registration[email]' => 'test@test.com',
+            'registration[password][first]' => 'test',
+            'registration[password][second]' => 'test',
+        ]);
+
+        $this->assertResponseStatusCodeSame(422);
+        $this->assertSelectorTextContains('.invalid-feedback', 'Cette chaîne est trop courte. Elle doit avoir au minimum 8 caractères.');
+    }
+
+    public function testRegistrationWithExistingEmail(): void
+    {
+        $client = static::createClient();
+        $container = static::$kernel->getContainer();
+
+        $entityManager = $container->get('doctrine')->getManager();
+        $existingUser = new User();
+        $existingUser->setEmail('existing@test.com');
+        $existingUser->setPassword('hashedpassword');
+        $entityManager->persist($existingUser);
+        $entityManager->flush();
+
+        $client->request('GET', '/registration');
+        $client->submitForm('signup-btn', [
+            'registration[email]' => 'existing@test.com',
+            'registration[password][first]' => 'testtest123',
+            'registration[password][second]' => 'testtest123',
+        ]);
+
+        $this->assertResponseStatusCodeSame(422);
+        $this->assertSelectorTextContains('.invalid-feedback', 'This email is already registered.'); // adapter
     }
 }
