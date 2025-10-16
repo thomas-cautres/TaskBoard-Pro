b8f1ca8e8dca0fad887bea3be6249bcb8fb5396d
Thomas Cautrès <thomas.cautres@protonmail.com>
2025-10-16 14:33:13 +0200

    --

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,11 +8,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class DashboardController extends AbstractController
 {
-    #[Route('/', name: 'home', methods: ['GET'])]
+    #[Route('/dashboard', name: 'dashboard', methods: ['GET'])]
     public function __invoke(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('dashboard.html.twig');
     }
 }
--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -13,13 +13,25 @@ class LoginTest extends WebTestCase
         $client = static::createClient();
         $client->request('GET', '/login');
         $this->assertResponseIsSuccessful();
+        $content = $client->getResponse()->getContent();
+
+        $this->assertStringContainsString('TaskBoard Pro', $content);
+        $this->assertStringContainsString('Accédez à votre espace', $content);
+        $this->assertStringContainsString('Email', $content);
+        $this->assertStringContainsString('Mot de passe', $content);
+        $this->assertStringContainsString('Se souvenir de moi', $content);
+        $this->assertStringContainsString('Se connecter', $content);
+        $this->assertStringContainsString('Mot de passe oublié ?', $content);
+        $this->assertStringContainsString('Pas encore de compte ?', $content);
+        $this->assertStringContainsString('S&#039;inscrire', $content);
+        $this->assertStringContainsString('Retour à l&#039;accueil', $content);
 
         $client->submitForm('login-btn', [
             '_username' => 'user-confirmed@domain.com',
             '_password' => 'test1234',
         ]);
 
-        $this->assertResponseRedirects('/');
+        $this->assertResponseRedirects('/dashboard');
     }
 
     public function testPasswordIsInvalid(): void
@@ -50,5 +62,21 @@ class LoginTest extends WebTestCase
 
         $client->followRedirect();
         $this->assertSelectorTextContains('.alert-danger', "Votre inscription n'est pas confirmée.");
+    }
+
+    public function testPageLinksWork(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $link = $crawler->filter('a[href="/registration"]')->link();
+        $client->click($link);
+        $this->assertResponseIsSuccessful();
+        $this->assertRouteSame('registration');
+
+        $link = $crawler->filter('a[href="/"]')->link();
+        $client->click($link);
+        $this->assertResponseIsSuccessful();
+        $this->assertRouteSame('home');
     }
 }
